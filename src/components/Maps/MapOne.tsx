dc3324829437010d59334aca98895a8d3429f195
Gaurav-Soni24 <sonigaurav2021@gmail.com>
2024-09-07 11:03:11 +0530

    first gaurav

--- src/components/Maps/MapOne.tsx
+++ src/components/Maps/MapOne.tsx
@@ -56,7 +56,7 @@ const AQIMarker: React.FC<{ data: AQIData }> = React.memo(({ data }) => {
   );
 
   return (
-    <Marker position={[data.lat, data.lon]} icon={customIcon as L.DivIcon}>
+    <Marker position={[data.lat, data.lon]} icon={customIcon}>
       <Popup>
         <strong>{data.station.name}</strong>
         <br />
