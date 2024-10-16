a0cc9641054b333bc3989282299a81f8b9e890b1
Navneet <nav301lll@gmail.com>
2024-10-16 18:41:05 +0530

    initial of awareness

---
+++ src/app/awareness/about/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Link } from 'react-router-dom';
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      about
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/awareness/guidance/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Link } from 'react-router-dom';
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      guidance
+    </div>
+  );
+};
+
+export default Page;
